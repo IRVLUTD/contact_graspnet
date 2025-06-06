ae10f05af3382815608d47c5ccb9fb8a498e3f0c
07hokage <saihaneesh.allu@gmail.com>
2025-06-06 12:54:18 -0500

    run cgnet without ros based on locally stored files

--- run_cgnet_without_ros.sh
+++ run_cgnet_without_ros.sh
@@ -6,4 +6,5 @@ set -e
 export PYTHONUNBUFFERED="True"
 
 
-python ./ros/runcgnet.py --local_regions --filter_grasps --forward_passes 4 --viz
+python ./ros/runcgnet.py --local_regions --filter_grasps --forward_passes 4 --viz --obj_pc_path "/home/robot-nav/obj_pc.npy"  --scene_pc_path "/home/robot-nav/scene_pc.npy"  --output_path "/home/robot-nav/grasps.npy"
+# python ./ros/runcgnet.py --local_regions --filter_grasps --forward_passes 4 --viz 
